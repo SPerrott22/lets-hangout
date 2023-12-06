499462c92d35685deb2174bad534de7f5f94fb91
SPerrott22 <56176775+SPerrott22@users.noreply.github.com>
2023-12-06 15:54:45 -0800

    Adjusted Home page

--- front-end/pages/Home.jsx
+++ front-end/pages/Home.jsx
@@ -5,6 +5,94 @@ import MailCheck from "/mail-check.svg";
 import CalendarBolt from "/calendar-bolt.svg";
 import "./Home.css";
 
+// function Home() {
+//   return (
+//     <div id="app" className="container my-4">
+//       <div id="introText" className="row justify-content-center">
+//         <div className="col-md-8">
+//           <div className="card shadow-sm">
+//             <div className="card-body">
+//               <h1 className="card-title text-center">Welcome!</h1>
+//               <div className="text-center">
+//                 <img
+//                   src={Picture}
+//                   alt="Olivia Wilson"
+//                   className="img-fluid rounded-circle my-3"
+//                   style={{ maxWidth: "300px" }}
+//                 />
+//               </div>
+//               <div id="description">
+//                 <div className="descriptionColumn">
+//                   <img src={Group} alt="Group" />
+//                   <p>Create groups with your friends</p>
+//                 </div>
+//                 <div className="descriptionColumn">
+//                   <img src={MailCheck} alt="Mail Check" />
+//                   <p>Invite groups to events</p>
+//                 </div>
+//                 <div className="descriptionColumn">
+//                   <img src={CalendarBolt} alt="Calendar Bolt" />
+//                   <p>See your events on a calendar</p>
+//                 </div>
+//               </div>
+//               <p className="card-text">
+//                 Schedule hangouts with your friends! Discover new places, make
+//                 plans, and enjoy your social life with ease. Let's Hangout is
+//                 your go-to app for connecting with friends and creating
+//                 memorable experiences.
+//               </p>
+//               <hr /> {/* Inserted horizontal line */}
+//               <p className="card-text">
+//                 <small className="text-muted">💙🐻💛 I love CS35L 💛🐻💙</small>
+//               </p>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+//   // =======
+//   // import React from 'react';
+
+//   // function Home() {
+
+//   // 	return (
+//   // 		<div id='app'>
+//   // 			<div id='introText'>
+//   // 				<p></p>
+//   // 				<h1>Welcome to Let's Hangout!</h1>
+//   // 				<p>
+//   // 					Schedule hangouts with your friends!
+//   // 				</p>
+//   // 			</div>
+//   // 			<div id='description'>
+//   // 				<div className='descriptionColumn'>
+//   // 					<img src={Group} alt="Group" />
+//   // 					<p>
+//   // 						Create groups with your friends
+//   // 					</p>
+//   // 				</div>
+//   // 				<div className='descriptionColumn'>
+//   // 					<img src={MailCheck} alt="Mail Check" />
+//   // 					<p>
+//   // 						Invite groups to events
+//   // 					</p>
+//   // 				</div>
+//   // 				<div className='descriptionColumn'>
+//   // 					<img src={CalendarBolt} alt="Calendar Bolt" />
+//   // 					<p>
+//   // 						See your events on a calendar
+//   // 					</p>
+//   // 				</div>
+
+//   // 			</div>
+//   // 		</div>
+//   // 	);
+//   // >>>>>>> home-page
+// }
+
+// export default Home;
+
 function Home() {
   return (
     <div id="app" className="container my-4">
@@ -18,7 +106,7 @@ function Home() {
                   src={Picture}
                   alt="Olivia Wilson"
                   className="img-fluid rounded-circle my-3"
-                  style={{ maxWidth: "300px" }}
+                  style={{ maxWidth: "200px" }}
                 />
               </div>
               <div id="description">
@@ -41,7 +129,7 @@ function Home() {
                 your go-to app for connecting with friends and creating
                 memorable experiences.
               </p>
-              <hr /> {/* Inserted horizontal line */}
+              <hr />
               <p className="card-text">
                 <small className="text-muted">💙🐻💛 I love CS35L 💛🐻💙</small>
               </p>
@@ -51,44 +139,6 @@ function Home() {
       </div>
     </div>
   );
-  // =======
-  // import React from 'react';
-
-  // function Home() {
-
-  // 	return (
-  // 		<div id='app'>
-  // 			<div id='introText'>
-  // 				<p></p>
-  // 				<h1>Welcome to Let's Hangout!</h1>
-  // 				<p>
-  // 					Schedule hangouts with your friends!
-  // 				</p>
-  // 			</div>
-  // 			<div id='description'>
-  // 				<div className='descriptionColumn'>
-  // 					<img src={Group} alt="Group" />
-  // 					<p>
-  // 						Create groups with your friends
-  // 					</p>
-  // 				</div>
-  // 				<div className='descriptionColumn'>
-  // 					<img src={MailCheck} alt="Mail Check" />
-  // 					<p>
-  // 						Invite groups to events
-  // 					</p>
-  // 				</div>
-  // 				<div className='descriptionColumn'>
-  // 					<img src={CalendarBolt} alt="Calendar Bolt" />
-  // 					<p>
-  // 						See your events on a calendar
-  // 					</p>
-  // 				</div>
-
-  // 			</div>
-  // 		</div>
-  // 	);
-  // >>>>>>> home-page
 }
 
 export default Home;
